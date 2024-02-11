ac619645df80225cd2dec8460daeed39340c1569
Your Name <you@example.com>
2024-02-11 00:13:10 +0000

    first commit

---
+++ src/main/java/com/chahboune/kafkaspringapptuto/KafkaSpringAppTutoApplication.java
@@ -0,0 +1,13 @@
+package com.chahboune.kafkaspringapptuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringAppTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringAppTutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chahboune/kafkaspringapptuto/KafkaSpringAppTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chahboune.kafkaspringapptuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringAppTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
